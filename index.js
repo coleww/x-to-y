e70f1d9189a54e9b8fd298a6c60f9a49611b9324
Cole Willsea <coleww@gmail.com>
2015-10-08 08:34:30 -0700

    nice

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-module.exports = function (str) {
-  return 'hello ' + str
+var Twit = require('twit')
+var pick = require('pick-random')
+var capitalize = require('capitalize')
+var addEnder = require('add-ender')
+var iscool = require('iscool')()
+var cleanThisTweetUp = require('clean-this-tweet-up')
+
+module.exports = function(x, y, config) {
+  var T = new Twit(config)
+  T.get('search/tweets', { q: x, count: 100, result_type: 'recent' }, function(err, data, response) {
+    var cleaned = data.statuses.map(cleanThisTweetUp).filter(function(t){
+      return t && !!t.match(x) && iscool(t)
+    })
+    var toot = pick(cleaned)[0]
+    toot = addEnder(cap(toot.replace(new RegExp(x, 'gi'), y)))
+    console.log(toot)
+    T.post('statuses/update', {status: toot}, function (err, data, response) {
+      console.log(err)
+      console.log(data)
+    })
+  })
 }
