65180961f03a0427efcf6f7d600fc9c6131c8b59
Cole Willsea <coleww@gmail.com>
2015-10-12 14:39:31 -0700

    nice hope that fixes the img bug

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ module.exports = function(x, y, lang, config) {
   T.get('search/tweets', { q: x, count: 100, result_type: 'recent', lang: lang}, function(err, data, response) { // grab 100 of the most recent tweets with this X
     var cleaned = data.statuses.map(cleanThisTweetUp).filter(function(t){ // remove usernames, links, hastags, etc.
       return t && !!t.match(x) && tipots(t) // filter out ones that don't actually contain x, or that are not cool
+    }).map(function(x){
+      return x.match(/[a-zA-Z0-9\s]+/g)[0] // umm hopefully this destroys the weird image bug
     })
     var toot = pick(cleaned)[0] // pick one at random
     toot = addEnder(cap(toot.replace(new RegExp(x, 'gi'), y))) // replace x with y, capitalize the tweet, and add a period if one is not present
