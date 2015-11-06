bb5016444592a6d22bd0209909ce3a3e64bf5713
Cole Willsea <coleww@gmail.com>
2015-11-06 06:51:36 -0800

    more, more!

--- index.js
+++ index.js
@@ -6,18 +6,19 @@ var tipots = require('this-is-probably-ok-to-say')
 var cleanThisTweetUp = require('clean-this-tweet-up')
 
 module.exports = function(x, y, lang, config) {
+  var reggie = '\b' + x + '\b'
   var T = new Twit(config)
   T.get('search/tweets', { q: x, count: 100, result_type: 'recent', lang: lang}, function(err, data, response) { // grab 100 of the most recent tweets with this X
     var cleaned = data.statuses.map(cleanThisTweetUp).filter(function(t){ // remove usernames, links, hastags, etc.
-      return t && !!t.match(x) && tipots(t) // filter out ones that don't actually contain x, or that are not cool
+      return t && !!t.match(reggie) && tipots(t) // filter out ones that don't actually contain x, or that are not cool
     }).map(function(x){
       return x.match(/[a-zA-Z0-9\s]+/g)[0] // umm hopefully this destroys the weird image bug
-    }).filter(function(y){
-      return y.match(x)
+    }).filter(function(t){
+      return !!t.match(reggie)
     })
     if (cleaned){
       var toot = pick(cleaned)[0] // pick one at random
-      toot = addEnder(cap(toot.replace(new RegExp(x, 'gi'), y))) // replace x with y, capitalize the tweet, and add a period if one is not present
+      toot = addEnder(cap(toot.replace(new RegExp(reggie, 'gi'), y))) // replace x with y, capitalize the tweet, and add a period if one is not present
       console.log(toot)
       if (toot.length < 140) { // if y is longer than x, then it is possible to make a too long tweet! that is not good!
         T.post('statuses/update', {status: toot}, function (err, data, response) { // tweet it out to the world!
